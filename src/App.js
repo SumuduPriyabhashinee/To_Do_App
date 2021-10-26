7792c1c128a13772a3e59a40d3ca785637007da5
Sumudu Priyabhashinee <59961195+SumuduPriyabhashinee@users.noreply.github.com>
2021-10-26 21:06:47 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './components/routes/Routes';
+import Navibar from './components/navibar/Navibar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navibar/>
         <Routes />
       </BrowserRouter>
     </div>
